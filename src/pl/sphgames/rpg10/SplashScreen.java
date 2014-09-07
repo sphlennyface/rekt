b6dc7d1379e592152e7b6cf5722ac4bd48b37ebc
sphlennyface <roiekt@gmail.com>
2014-09-07 21:19:36 +0200

    also lekki improve położenia gandalfa po lvlswitchu
    ^

--- src/pl/sphgames/rpg10/SplashScreen.java
+++ src/pl/sphgames/rpg10/SplashScreen.java
@@ -70,6 +70,13 @@ public class SplashScreen {
 		if (frames == 20 && level == true) {
 			changeLevelm8();
 		}
+		
+		 try {
+		        Thread.sleep(20);
+		    } catch (InterruptedException e) {
+
+		        e.printStackTrace();
+		    }
       
       
 	}
